a0a6d09d0295ae666be5acc3b8ba27a1128dff8a
unbtorsten <torsten.reuschel@unb.ca>
2022-02-02 21:29:59 -0400

    amend script wrt reproducibility, refactor for userspace

--- makeit.sh
+++ makeit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 PROJECT_NAME=blinky
-PREFIX=/usr/local/share
+PREFIX=~/opt
+PATH=$PATH:$PREFIX/nextpnr/bin
 DB_DIR=$PREFIX/nextpnr/prjxray-db
 CHIPDB_DIR=$PREFIX/nextpnr/xilinx-chipdb
 # QMTech XC7K32T board
 BOARD=qmtech
-# Genesys2 board
+# Digilent Genesys2 board
 # BOARD=genesys2
 
 if [[ "$BOARD" == "qmtech" ]]
@@ -23,8 +24,10 @@ fi
 set -ex
 yosys -p "synth_xilinx -flatten -abc9 -nobram -arch xc7 -top ${PROJECT_NAME}; write_json ${PROJECT_NAME}.json" ${PROJECT_NAME}.v
 nextpnr-xilinx --chipdb ${CHIPDB_DIR}/${PART}.bin --xdc ${PROJECT_NAME}-${BOARD}.xdc --json ${PROJECT_NAME}.json --write ${PROJECT_NAME}_routed.json --fasm ${PROJECT_NAME}.fasm --verbose --debug
-fasm2frames --part ${PART} --db-root ${DB_DIR}/kintex7 ${PROJECT_NAME}.fasm > ${PROJECT_NAME}.frames
-xc7frames2bit --part_file ${DB_DIR}/kintex7/${PART}/part.yaml --part_name ${PART} --frm_file ${PROJECT_NAME}.frames --output_file ${PROJECT_NAME}.bit
+source "${XRAY_DIR}/utils/environment.sh"
+${XRAY_UTILS_DIR}/fasm2frames.py --part ${PART} --db-root ${DB_DIR}/kintex7 ${PROJECT_NAME}.fasm > ${PROJECT_NAME}.frames
+echo ${XRAY_UTILS_DIR}
+${XRAY_TOOLS_DIR}/xc7frames2bit --part_file ${DB_DIR}/kintex7/${PART}/part.yaml --part_name ${PART} --frm_file ${PROJECT_NAME}.frames --output_file ${PROJECT_NAME}.bit
 #To send to SRAM:
 #openFPGALoader --board xxx ${PROJECT_NAME}.bit
 #To send to FLASH:
