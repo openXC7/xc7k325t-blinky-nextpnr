0bd0843890281411775857ba0bc6ee78f7f3dcfa
Torsten Reuschel <89043237+unbtorsten@users.noreply.github.com>
2022-02-21 16:33:15 -0400

    Update blinky.v
    fix and close #22

--- blinky.v
+++ blinky.v
@@ -1,13 +1,13 @@
 `default_nettype none   //do not allow undeclared wires
 
 module blinky (
-    input  wire i_clk,
-    output wire o_led
+    input  wire clk,
+    output wire led
     );
     
     reg [24:0] r_count = 0;
     
-    always @(posedge(i_clk)) r_count <= r_count + 1;
+    always @(posedge(clk)) r_count <= r_count + 1;
      
-    assign o_led = r_count[24];
+    assign led = r_count[24];
 endmodule
