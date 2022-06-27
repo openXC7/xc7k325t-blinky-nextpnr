bc7ee42aaae88462825eed1350489168b8668722
Hans Baier <hansfbaier@gmail.com>
2022-06-27 07:40:18 +0700

    whitespace

--- blinky.v
+++ blinky.v
@@ -4,10 +4,10 @@ module blinky (
     input  wire clk,
     output wire led
     );
-    
+
     reg [24:0] r_count = 0;
-    
+
     always @(posedge(clk)) r_count <= r_count + 1;
-     
+
     assign led = r_count[24];
 endmodule
