583c70f91c9265547dbaa6aba772086883650511
Hans Baier <hansfbaier@gmail.com>
2022-06-29 07:54:42 +0700

    differential output works

--- blinky-diffclk.v
+++ blinky-diffclk.v
@@ -3,11 +3,13 @@
 module blinky (
     input  wire clk_p,
     input  wire clk_n,
-    output wire led
+    output wire led_p,
+    output wire led_n,
     );
 
     wire clk_ibufg;
     wire clk;
+    wire led;
 
     IBUFDS ibuf_inst (.I(clk_p), .IB(clk_n), .O(clk_ibufg));
     BUFG   bufg_inst (.I(clk_ibufg), .O(clk));
@@ -17,4 +19,7 @@ module blinky (
     always @(posedge(clk)) r_count <= r_count + 1;
 
     assign led = r_count[24];
+
+    OBUFDS obuf_inst (.I(led), .O(led_p), .OB(led_n));
+
 endmodule
--- blinky-stlv7325.xdc
+++ blinky-stlv7325.xdc
@@ -5,5 +5,8 @@ set_property IOSTANDARD DIFF_SSTL15 [get_ports {clk_p}]
 set_property LOC AC11 [get_ports clk_n]
 set_property IOSTANDARD DIFF_SSTL15 [get_ports {clk_n}]
 
-set_property LOC AA2 [get_ports led]
-set_property IOSTANDARD LVCMOS15 [get_ports {led}]
+set_property LOC V11 [get_ports led_p]
+set_property IOSTANDARD LVCMOS15 [get_ports {led_p}]
+
+set_property LOC W11 [get_ports led_n]
+set_property IOSTANDARD LVCMOS15 [get_ports {led_n}]
